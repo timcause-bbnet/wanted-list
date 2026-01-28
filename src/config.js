3a9195a6a1ba608ed3c7bdae54e55051bf511682
brian <timcause@gmail.com>
2026-01-29 03:58:41 +0800

    config: set default firebase database url

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // 例如: "https://your-project-id.firebaseio.com"
 // 填入後，所有訪客將直接連線到此資料庫，無需再手動設定。
 
-export const DEFAULT_DB_URL = "";
+export const DEFAULT_DB_URL = "https://wanted-list-c2707-default-rtdb.firebaseio.com/";
