3726e721f4f85e24f7ece8a5617de0886ac8e478
brian <timcause@gmail.com>
2026-01-29 01:51:52 +0800

    fix: set base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // Set base if deploying to GitHub Pages user/repo, usually it's needed but I'll leave it default for now or add a comment
-    // base: '/wanted-list/', 
+    // Set base if deploying to GitHub Pages user/repo
+    base: '/wanted-list/',
 })
